43d41ba2d28316050f6ed6b2efbbb3812e83635f
Benedict Etzel <developer@beheh.de>
2014-12-22 00:44:09 +0100

    add grouping to Scorer

--- src/de/beheh/warlight2/stats/Scorer.java
+++ src/de/beheh/warlight2/stats/Scorer.java
@@ -11,9 +11,18 @@ public abstract class Scorer<E> implements Comparator<E> {
 
 	protected abstract double score(E object);
 
+	protected int group(E object) {
+		return 1;
+	}
+
 	@Override
 	public int compare(E o1, E o2) {
-		return Double.compare(score(o1), score(o2));
+		int group1 = group(o1);
+		int group2 = group(o2);
+		if (group1 != group2) {
+			return Integer.compare(group1, group2);
+		}
+		return Double.compare(score(o2), score(o1));
 	}
 
 }
